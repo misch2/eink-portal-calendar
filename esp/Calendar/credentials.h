a0027ecbffd97aa12bb85aeb6b4b52728537420c
Michal Schwarz <michal.schwarz@gmail.com>
2023-01-18 20:09:46 +0100

    empty credentials

---
+++ esp/Calendar/credentials.h
@@ -0,0 +1,8 @@
+#ifndef CREDENTIALS_H
+#define CREDENTIALS_H
+
+// Replace with your actual SSID and password:
+#define WIFI_SSID "Your SSID here"
+#define WIFI_PASSWD "WLAN AP password here"
+
+#endif
