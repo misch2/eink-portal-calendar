044dc0a1154ed3b668a2b0c018e21c5356d80a41
Michal Schwarz <michal.schwarz@gmail.com>
2023-02-14 21:19:01 +0100

    tidy up

--- server/lib/PortalCalendar/Config.pm
+++ server/lib/PortalCalendar/Config.pm
@@ -1,16 +1,42 @@
 package PortalCalendar::Config;
 
 use Mojo::Base -base;
+
 use DDP;
 
 has 'app';
+
+has 'defaults' => sub {
+    my $self = shift;
+    return {
+        sleep_time              => 3600,
+        broken_glass            => 0,
+        web_calendar1           => 0,
+        web_calendar2           => 0,
+        web_calendar3           => 0,
+        web_calendar_ics_url1   => '',
+        web_calendar_ics_url2   => '',
+        web_calendar_ics_url3   => '',
+        totally_random_icon     => 0,
+        min_random_icons        => 4,
+        max_random_icons        => 10,
+        max_icons_with_calendar => 5,
+    };
+};
+
+has 'parameters' => sub {
+    my $self = shift;
+    return [ keys %{ $self->defaults } ];
+};
 
 sub get {
     my $self = shift;
     my $name = shift;
 
-    my $item = $self->app->schema->resultset('Config')->search({ name => $name })->first;
-    return $item && $item->value;
+    my $item = $self->app->schema->resultset('Config')->find({ name => $name });
+
+    return $item->value if $item;
+    return $self->defaults->{$name};
 }
 
 sub set {
@@ -18,10 +44,15 @@ sub set {
     my $name  = shift;
     my $value = shift;
 
-    my $orig = $self->app->schema->resultset('Config')->search({ name => $name })->first;
-    $orig->value($value);
-    $orig->update();
-
+    $self->app->schema->resultset('Config')->update_or_create(
+        {
+            name  => $name,
+            value => $value,
+        },
+        {
+            key => 'name_unique',
+        }
+    );
     return;
 }
 
