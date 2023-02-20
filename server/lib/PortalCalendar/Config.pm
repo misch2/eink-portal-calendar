55bdde459e1ca2c3409a28bb74722e4f0c421208
Michal Schwarz <michal.schwarz@gmail.com>
2023-02-20 19:40:39 +0100

    better boolean parameters

--- server/lib/PortalCalendar/Config.pm
+++ server/lib/PortalCalendar/Config.pm
@@ -3,6 +3,7 @@ package PortalCalendar::Config;
 use Mojo::Base -base;
 
 use DDP;
+use List::Util qw(any);
 
 has 'app';
 
@@ -22,13 +23,36 @@ has 'defaults' => sub {
         max_random_icons        => 10,
         max_icons_with_calendar => 5,
         theme                   => 'portal_calendar',
+        openweather             => 0,
+        openweather_api_key     => '',
     };
+};
+
+has boolean_parameters => sub {
+    return [
+        qw/
+            totally_random_icon
+            broken_glass
+            web_calendar1
+            web_calendar2
+            web_calendar3
+            openweather
+            /
+    ];
 };
 
 has 'parameters' => sub {
     my $self = shift;
     return [ keys %{ $self->defaults } ];
 };
+
+sub is_boolean_parameter {
+    my $self = shift;
+    my $name = shift;
+
+    return 1 if any { $_ eq $name } @{ $self->boolean_parameters };
+    return 0;
+}
 
 sub get {
     my $self = shift;
@@ -44,6 +68,8 @@ sub set {
     my $self  = shift;
     my $name  = shift;
     my $value = shift;
+
+    $value //= 0 if $self->is_boolean_parameter($name);
 
     $self->app->schema->resultset('Config')->update_or_create(
         {
