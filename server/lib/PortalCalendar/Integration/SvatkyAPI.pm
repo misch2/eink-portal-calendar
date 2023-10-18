91f58d5705d80d037d5968d06faef20181338ddb
Michal Schwarz <michal.schwarz@gmail.com>
2023-10-18 19:04:25 +0200

    simplify

--- server/lib/PortalCalendar/Integration/SvatkyAPI.pm
+++ server/lib/PortalCalendar/Integration/SvatkyAPI.pm
@@ -3,56 +3,26 @@ package PortalCalendar::Integration::SvatkyAPI;
 use base qw/PortalCalendar::Integration/;
 
 use Mojo::Base -base;
-use Mojo::JSON qw(decode_json encode_json);
+use Mojo::JSON qw(decode_json);
 use Mojo::URL;
-use Mojo::File;
 use PortalCalendar::DatabaseCache;
 
-use LWP::UserAgent::Cached;
-use iCal::Parser;
-use DDP;
 use DateTime;
 use Time::Seconds;
 
 has 'lwp_max_cache_age' => 8 * ONE_HOUR;
 
 sub get_today_details {
-    my $self   = shift;
-    my $date   = shift // DateTime->now();
-    my $forced = shift;
+    my $self = shift;
+    my $date = shift // DateTime->now();
 
-    my $cache = PortalCalendar::DatabaseCache->new(app => $self->app);
-    my $data  = $cache->get_or_set(
-        sub {
-            my $url = Mojo::URL->new('https://svatkyapi.cz/api/day/' . $date->ymd('-'))->to_unsafe_string;
+    my $url      = Mojo::URL->new('https://svatkyapi.cz/api/day/' . $date->ymd('-'))->to_unsafe_string;
+    my $response = $self->caching_ua->get($url);
 
-            $self->app->log->debug($url);
-            my $response = $self->caching_ua->get($url);
+    die $response->status_line . "\n" . $response->content
+        unless $response->is_success;
 
-            die $response->status_line . "\n" . $response->content
-                unless $response->is_success;
-
-            return decode_json($response->decoded_content);
-        },
-        $self->db_cache_id . "/" . $date->ymd,
-        $forced
-    );
-
-    # {
-    #     date          "2023-10-15" (dualvar: 2023),
-    #     dayInWeek     "neděle",
-    #     dayNumber     15,
-    #     holidayName   undef,
-    #     isHoliday     0 (JSON::PP::Boolean) (read-only),
-    #     month         {
-    #         genitive     "října",
-    #         nominative   "říjen"
-    #     },
-    #     monthNumber   10,
-    #     name          "Tereza",
-    #     year          2023
-    # }
-    return $data;
+    return decode_json($response->decoded_content);
 }
 
 1;
