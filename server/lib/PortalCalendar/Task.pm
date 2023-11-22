fb47f75d7727e93de54811747197c28d5d319665
Michal Schwarz <michal.schwarz@gmail.com>
2023-11-22 19:51:19 +0100

    quick optimization

--- server/lib/PortalCalendar/Task.pm
+++ server/lib/PortalCalendar/Task.pm
@@ -33,6 +33,22 @@ sub regenerate_image {
     my $display = $job->app->get_display_by_id($display_id);
     $job->app->log->info("Regenerating calendar image for display #" . $display->id);
 
+    # FIXME don't just request "/calendar/id/html" via web, because that will trigger a refresh of the calendar IN THE WEBSERVER (UI thread) and not in this worker thread. And it causes errors like "Worker 1837132 has no heartbeat (50 seconds), restarting" when the UI thread is busy with getting calendar data etc..
+    # It would be much better to do all the calculations here and only use the "/html" route to get the HTML code using cached-only data.
+    # I.e. something like:
+    #   - minion job:
+    #      - fetch all the data from web and do all the calculations (calendars, weather, etc.)
+    #      - store the data in the cache (ideally with something like "force cache expiry to be at least now + 5 minutes" to prevent race condition)
+    #      - call the "/html" route to get the HTML code
+    #   - UI thread:
+    #      - just return the HTML code from the cache (i.e. with a "forced cache hit" option)
+
+    # For now we just do the calculations here in the worker thread and we hope the result will be cached for the next 5 minutes (see FIXME above)
+    $job->app->log->debug("prefetching cached data");
+    my $util = PortalCalendar::Util->new(app => $job->app, display => $display);
+    $util->html_for_date(DateTime->now(time_zone => $display->get_config('timezone')));    # FIXME use "forced cache expiry + N " option
+
+    $job->app->log->debug("generating img from cached data");
     my $converter = PortalCalendar::Web2Png->new(pageres_command => $job->app->home->child("node_modules/.bin/pageres"));
     $converter->convert_url($job->app->config->{url_start} . '/calendar/' . $display->id . '/html', $display->virtual_width, $display->virtual_height, $job->app->home->child("generated_images/current_calendar_" . $display->id . ".png"));
 
