2e712cc60ca5d8e88260d475ec678e124840d3fc
Michal Schwarz <michal.schwarz@gmail.com>
2026-02-19 14:47:51 +0100

    improve appsettings handling

--- server/PortalCalendarServer/Program.cs
+++ server/PortalCalendarServer/Program.cs
@@ -28,9 +28,12 @@ builder.Logging.AddSimpleConsole(options =>
 
 // Configure the SQLite connection string to use an absolute path
 var rawConnectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-var relativePath = rawConnectionString!.Replace("Data Source=", "");
-var absolutePath = Path.Combine(builder.Environment.ContentRootPath, relativePath);
-absolutePath = Path.GetFullPath(absolutePath); // Ensure it's an absolute path
+
+var templatePath = rawConnectionString!.Replace("Data Source=", "");
+var realPath = templatePath;
+realPath = realPath.Replace("{ContentRootPath}", builder.Environment.ContentRootPath);
+var absolutePath = Path.GetFullPath(realPath); // Ensure it's an absolute path
+
 var absoluteConnectionString = $"Data Source={absolutePath}";
 builder.Services.AddDbContext<CalendarContext>(options =>
     options.UseLazyLoadingProxies()
