363487a291608c517ba6b054a88b4ad3563cd1b0
Michal Schwarz <michal.schwarz@gmail.com>
2026-03-09 11:00:11 +0100

    fix #defines and add memory saving option

--- client/include/epaper/GDEW075Z08_3C_BWR.h
+++ client/include/epaper/GDEW075Z08_3C_BWR.h
@@ -1,7 +1,7 @@
 // 7.5" 3C 800x480 B/W/R
 // https://www.aliexpress.com/item/1005005121813674.html
 
-#define DISPLAY_CLASS_TYPE GxEPD2_3C<GxEPD2_750c_Z08, GxEPD2_750c_Z08::HEIGHT>  // or HEIGHT/2 etc. for partial page buffer and memory savings
+#define DISPLAY_CLASS_TYPE GxEPD2_3C<GxEPD2_750c_Z08, GxEPD2_750c_Z08::HEIGHT / SPLIT_DISPLAY_INTO_N_PAGES>
 #define DISPLAY_CLASS_ARGUMENTS (GxEPD2_750c_Z08(CS_PIN, DC_PIN, RST_PIN, BUSY_PIN))
 
 #define DISPLAY_WIDTH 800
--- client/include/epaper/GDEY075Z08_3C_BWR_inverted_red.h
+++ client/include/epaper/GDEY075Z08_3C_BWR_inverted_red.h
@@ -2,8 +2,7 @@
 // "SUNMAXIC 7.5 inch Electronic Paper Ink Screen 800x480 Resolution Black And White EPD E-Paper UC8179 Driver SPI Interface 24Pin"
 // https://www.aliexpress.com/item/1005007175851007.html
 
-#define DISPLAY_CLASS_TYPE \
-  GxEPD2_3C<GxEPD2_750c_GDEY075Z08_inverted, GxEPD2_750c_GDEY075Z08_inverted::HEIGHT>  // or HEIGHT/2 etc. for partial page buffer and memory savings
+#define DISPLAY_CLASS_TYPE GxEPD2_3C<GxEPD2_750c_GDEY075Z08_inverted, GxEPD2_750c_GDEY075Z08_inverted::HEIGHT / SPLIT_DISPLAY_INTO_N_PAGES>
 #define DISPLAY_CLASS_ARGUMENTS (GxEPD2_750c_GDEY075Z08_inverted(CS_PIN, DC_PIN, RST_PIN, BUSY_PIN))
 
 #define DISPLAY_WIDTH 800
