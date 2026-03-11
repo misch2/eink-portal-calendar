855582d3bae3c04524c1b255d58f1474dad69e56
Michal Schwarz <michal.schwarz@gmail.com>
2026-03-11 20:16:34 +0100

    revert debug code

--- client/include/boards/michal/epaper7/board.h
+++ client/include/boards/michal/epaper7/board.h
@@ -7,14 +7,11 @@
 #include "epaper/GDEM075F52_4C_BWRY.h"
 
 #define USE_WIFI_MANAGER
-// #define USE_WDT
-// #define WDT_TIMEOUT 120  // seconds
+#define USE_WDT
+#define WDT_TIMEOUT 120  // seconds
 
-#define HOSTNAME "esp65-epaper7" /* a board number, not a chip ID */
-
-// FIXME test only
-#define USE_MDNS_FOR_SERVER
-// #define CALENDAR_URL_HOST "portal-calendar.localnet" /* .NET server on Proxmox LXC */
-// #define CALENDAR_URL_PORT 8084                       /* nginx port to log requests, real server runs on 5000 */
+#define HOSTNAME "esp65-epaper7"                     /* a board number, not a chip ID */
+#define CALENDAR_URL_HOST "portal-calendar.localnet" /* .NET server on Proxmox LXC */
+#define CALENDAR_URL_PORT 8084                       /* nginx port to log requests, real server runs on 5000 */
 
 #define DISPLAY_ROTATION 2  // see hw_config.h for details
