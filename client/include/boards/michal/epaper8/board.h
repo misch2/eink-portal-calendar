5728f7357b7dff8a888d873dfce4c890d36160e6
Michal Schwarz <michal.schwarz@gmail.com>
2026-03-26 08:36:07 +0100

    add LaskaKit ESP32-LPkit board

--- client/include/boards/michal/epaper8/board.h
+++ client/include/boards/michal/epaper8/board.h
@@ -3,14 +3,14 @@
 #define SYSLOG_PORT 514
 #define SYSLOG_MYAPPNAME "epaper"
 
-#include "driver/esp32s3_generic_plus_despic73.h"
+#include "driver/laskakit_esp32-lpkit_v2_4_plus_despic73.h"
 #include "epaper/GDEP073E01_6C.h"
 
 #define USE_WIFI_MANAGER
-#define USE_WDT
-#define WDT_TIMEOUT 120  // seconds
+// #define USE_WDT
+// #define WDT_TIMEOUT 120  // seconds
 
-#define HOSTNAME "esp30-epaper8"                     /* a board number, not a chip ID */
+#define HOSTNAME "esp68-epaper8"                     /* a board number, not a chip ID */
 #define CALENDAR_URL_HOST "portal-calendar.localnet" /* .NET server on Proxmox LXC */
 #define CALENDAR_URL_PORT 8084                       /* nginx port to log requests, real server runs on 5000 */
 
