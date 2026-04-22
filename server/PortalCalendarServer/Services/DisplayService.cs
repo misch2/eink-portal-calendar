1bfb1e225ff9b76fe443b091a14ef4a43b19818b
Michal Schwarz <michal.schwarz@gmail.com>
2026-04-22 21:08:11 +0200

    remove jjn dithering, add localization here too

--- server/PortalCalendarServer/Services/DisplayService.cs
+++ server/PortalCalendarServer/Services/DisplayService.cs
@@ -557,7 +557,6 @@ public class DisplayService(
             {
                 "fs" => KnownDitherings.FloydSteinberg,
                 "at" => KnownDitherings.Atkinson,
-                "jjn" => KnownDitherings.JarvisJudiceNinke,
                 "st" => KnownDitherings.Stucki,
                 _ => null
             };
